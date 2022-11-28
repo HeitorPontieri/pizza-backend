a8f479acdf046ac4644af610f9f8a4b75be9e629
HeitorPontieri <98404376+HeitorPontieri@users.noreply.github.com>
2022-11-28 09:36:46 -0300

    DAO and controller add
    Co-Authored-By: Lucas Camilo <98404472+LusCamilo@users.noreply.github.com>

---
+++ controller/controllerIngredientes.js
@@ -0,0 +1,8 @@
+/*
+//////////////// DOCUMENTAÇÃO \\\\\\\\\\\\\\\\\\\\\
+Objetivo : Arquivo responsável pela manipulação de recebimento, tratamento e retorno de dados
+entre a API e a model
+Autor : HeitorPontieri
+Data_criação : 28/11/2022
+Versão : 1.0
+*/
---
+++ model/dao/bebida.js
@@ -0,0 +1,9 @@
+
+
+/*
+//////////////// DOCUMENTAÇÃO \\\\\\\\\\\\\\\\\\\\\
+Objetivo : Arquivo responsável pela manipulação de dados com o banco de dados (insert,update,delete,select)
+Autor : HeitorPontieri
+Data_criação : 28/10/2022
+Versão : 1.0
+*/
